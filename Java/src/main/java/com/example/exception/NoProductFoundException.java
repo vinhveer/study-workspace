7062838522318b8f301c6b55c341db83f8a6d484
Vinh Veer <vinhveer123@gmail.com>
2024-06-21 03:23:17 +0700

    Upload

--- Java/src/main/java/com/example/exception/NoProductFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.exception;
-
-public class NoProductFoundException extends Exception{
-//    private static final long seriaLVersionUID = 1L;
-    public NoProductFoundException(String message) {
-        super(message);
-    }
-}
--- Java/src/main/java/com/example/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repo;
-
-import com.example.entity.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, Long> {
-}
